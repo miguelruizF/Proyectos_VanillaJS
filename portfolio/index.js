eab16c975c18673831af71be152bc40882e5dbb4
Miguel Ruiz <later481@gmail.com>
2024-03-23 21:59:27 -0600

    project finish

--- portfolio/index.js
+++ portfolio/index.js
@@ -4,3 +4,16 @@ const header = d.querySelector("header");
 window.addEventListener("scroll", ()=>{
     header.classList.toggle("sticky", window.scrollY > 140);
 })
+
+let menu = d.querySelector("#menu-icon");
+let nav = d.querySelector(".nav");
+
+menu.onclick = () => {
+    menu.classList.toggle("bx-x");
+    nav.classList.toggle("open");
+}
+
+window.onscroll = () => {
+    menu.classList.remove("bx-x");
+    nav.classList.remove("open");
+}
