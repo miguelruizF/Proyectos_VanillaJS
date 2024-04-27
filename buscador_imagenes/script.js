1d468f09a37735e3ddc49668d131c949c237342f
Miguel Ruiz <later481@gmail.com>
2024-04-27 00:41:03 -0600

    cambios ui tamaño cards

--- buscador_imagenes/script.js
+++ buscador_imagenes/script.js
@@ -85,7 +85,7 @@ function mostrarImagenes(imagenes) {
         resultado.innerHTML += `
             <div class="w-1/2 md:w-1/3 lg:w-1/4 p-2 mb-4">
                 <div class="bg-white">
-                    <img class="w-full" src="${previewURL}">
+                    <img class="w-full h-1/2" src="${previewURL}">
                     <div class="p-4">
                         <p class="font-bold">${likes} <span class="font-light">Me gusta</span></p>
                         <p class="font-bold">${views} <span class="font-light">Veces vistas</span></p>
